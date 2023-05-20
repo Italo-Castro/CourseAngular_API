111c1815ad6deec2b4f661e3348623d2d3654cd9
Italo Castro <italocastro2010@gmail.com>
2023-05-20 11:27:06 -0300

    Adicionando validação de formulario

--- moments/src/app/components/moment-form/moment-form.component.ts
+++ moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,10 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
   styleUrls: ['./moment-form.component.css'],
 })
-export class MomentFormComponent {
+export class MomentFormComponent implements OnInit {
   @Input() btnText!: string;
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  momentForm!: FormGroup;
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+    console.log('enviou o formulario');
+  }
 }
