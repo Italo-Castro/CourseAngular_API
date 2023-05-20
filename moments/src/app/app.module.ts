232b1380b2836331b864d77c6f70e5cf15686320
Italo Castro <italocastro2010@gmail.com>
2023-05-20 10:25:39 -0300

    Components Header e Footer

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './comonets/header/header.component';
+import { FooterComponent } from './comonets/footer/footer.component';
+import { AboutComponent } from './components/page/about/about.component';
+import { HomeComponent } from './components/page/home/home.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, AboutComponent, HomeComponent, NewMomentComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
